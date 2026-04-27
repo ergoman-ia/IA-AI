a1149cbe804265bd44250bf7bb62303c0e4401a9
ergoman-ia <ergoman@kakao.com>
2026-04-27 09:24:52 +0900

    Add files via upload

---
+++ secrets.js
@@ -0,0 +1,9 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDjrDquFFaymlGYvLG-MiVZUAfGweoBjsE",
+  authDomain: "hep2-a122c.firebaseapp.com",
+  projectId: "hep2-a122c",
+  storageBucket: "hep2-a122c.firebasestorage.app",
+  messagingSenderId: "776240221625",
+  appId: "1:776240221625:web:006cf245ca81a00fefba68"
+};
+export const ADMIN_PASSWORD = "admin_0482";
